af271651a2f05591f4ce92bd01d983a95009c601
elijah2023-dev <elijahmugo697@gmail.com>
2026-02-21 20:17:01 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================= typing animation ======================= */
+var typed = new Typed(".typing",{
+    strings:["","Graphic Designer","graphic Designer","Web Developer","Vedio Editer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
